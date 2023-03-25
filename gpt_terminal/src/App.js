f4992022b4e0574b95ca1066793866c6f0aa7057
Tom Dörr <tomdoerr96@gmail.com>
2023-03-25 03:47:10 +0100

    Refactor terminal output and add directory navigation support

--- gpt_terminal/src/App.js
+++ gpt_terminal/src/App.js
@@ -1,45 +1,76 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./App.css";
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [command, setCommand] = useState("");
-  const [output, setOutput] = useState("");
+  const [history, setHistory] = useState([]);
+  const [currentCommand, setCurrentCommand] = useState('');
+  const terminalOutputRef = useRef();
 
-  const executeCommand = async () => {
+  const executeCommand = async (command) => {
     try {
-      const { data } = await axios.post("http://localhost:3001/execute", { command });
-      setOutput(data.result);
+      const { data } = await axios.post('http://localhost:3001/execute', { command });
+      setHistory([...history, { command, output: data.result }]);
     } catch (error) {
-      setOutput(`Error: ${error.message}`);
+      setHistory([...history, { command, output: `Error: ${error.message}` }]);
     }
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      executeCommand();
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const command = currentCommand.trim();
+      if (command) {
+        executeCommand(command);
+        setCurrentCommand('');
+      }
     }
   };
+
+const handleFocus = (event) => {
+  terminalOutputRef.current.scrollTo(0, terminalOutputRef.current.scrollHeight);
+  if (event.type === 'click') {
+    setCurrentCommand(event.target.textContent);
+  }
+};
+
+  const displayHistory = history.map((entry, index) => (
+    <React.Fragment key={index}>
+      <span>{`$ ${entry.command}`}</span>
+      <br />
+      <span>{entry.output}</span>
+      <br />
+      <br />
+    </React.Fragment>
+  ));
 
   return (
     <div className="App">
       <h1>React Terminal</h1>
-      <textarea
-        rows="15"
-        cols="80"
-        value={output}
-        readOnly
-      />
-      <input
-        type="text"
-        value={command}
-        onChange={(e) => setCommand(e.target.value)}
-        onKeyPress={handleKeyPress}
-        placeholder="Enter command"
-      />
-      <button onClick={executeCommand}>Execute</button>
-      </div>
+<div
+  className="terminal-output"
+  ref={terminalOutputRef}
+  style={{
+    whiteSpace: 'pre-wrap',
+    width: '80%',
+    minHeight: '300px',
+    maxHeight: '500px',
+    overflowY: 'auto',
+    padding: '10px',
+    backgroundColor: 'black',
+    color: 'white',
+    border: '1px solid #61dafb',
+    marginBottom: '10px',
+  }}
+  onClick={handleFocus}
+  onFocus={handleFocus} // Add this line
+  contentEditable
+  onKeyPress={handleKeyPress}
+>
+  {displayHistory}
+</div>
+    </div>
   );
 }
+
 export default App;
-   
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const { exec } = require("child_process");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
+let currentDir = process.cwd();
 
 app.post("/execute", (req, res) => {
   const { command } = req.body;
@@ -15,15 +18,29 @@ app.post("/execute", (req, res) => {
     return res.status(400).json({ error: "Command is required" });
   }
 
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({ error: stderr });
+  if (command.startsWith("cd ")) {
+    const newPath = path.join(currentDir, command.slice(3));
+    try {
+      process.chdir(newPath);
+      currentDir = process.cwd();
+      res.json({ result: `Changed directory to ${currentDir}` });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-    res.json({ result: stdout });
-  });
+ } else {
+    exec(command, { cwd: currentDir }, (error, stdout, stderr) => {
+      if (stderr) {
+        return res.json({ result: stderr });
+      }
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
+      res.json({ result: stdout });
+    });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port
+  ${PORT}`);
 });
-
